cdf09fa73a6f5f3844b63d075c7e4ac1ed91aafd
Christopher Sesugh Aondona <christohybrid185@gmail.com>
2022-12-08 14:29:29 +0100

    refactor: resturcture project structure

--- server/src/libs/stripe.js
+++
@@ -1,6 +0,0 @@
-import Stripe from "stripe";
-
-const apiKey = process.env.STRIPE_SECRET_KEY;
-export const stripe = new Stripe(apiKey, {
-  apiVersion: "2022-08-01",
-});
--- server/src/routes/subscription.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { subscribe } from "../controllers/subscription.js";
-import auth from "../middleware/authebtication.js";
-
-const router = express.Router();
-
-router.get("/", subscribe);
-
-export default router;
