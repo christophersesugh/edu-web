f9af3821ff68f1e09fc47bff17723af00a4d8e6c
Christopher Sesugh Aondona <christohybrid185@gmail.com>
2022-12-09 11:10:17 +0100

    feat: add project structure

---
+++ src/auth-app/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AuthenticatedApp() {
+  return <div>index</div>;
+}
---
+++ src/unauth-app/unauth-routes.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UnauthRoutes() {
+  return <div>unauth-routes</div>;
+}
