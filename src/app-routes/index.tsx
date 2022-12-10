0876e5e8cba80449dfc8de050491b9124438bee6
Christopher Sesugh Aondona <christohybrid185@gmail.com>
2022-12-10 10:36:08 +0100

    refactor: restructure app-routes

--- src/app-routes/index.tsx
+++ src/app-routes/index.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Admin, Course, Courses, Home, NotFound } from "../screens";
+import {
+  Admin,
+  BinaryTrees,
+  Course,
+  Courses,
+  Functions,
+  Home,
+  LinkedLists,
+  NotFound,
+  Numbers,
+  Stack,
+} from "../screens";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/courses" element={<Courses />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/course/courses" element={<Courses />} />
       <Route path="/course/:id" element={<Course />} />
+      <Route path="/course/stack" element={<Stack />} />
+      <Route path="/course/binary-trees" element={<BinaryTrees />} />
+      <Route path="/course/functions" element={<Functions />} />
+      <Route path="/course/linked-lists" element={<LinkedLists />} />
+      <Route path="/course/numbers" element={<Numbers />} />
       <Route path="*" element={<NotFound />} />
-      {/* <Route path="/stack" element={<Stack />} />
-      <Route path="/binary-trees" element={<BinaryTrees />} />
-      <Route path="/functions" element={<Functions />} />
-      <Route path="/linked-lists" element={<LinkedLists />} />
-      <Route path="/numbers" element={<Numbers />} /> */}
     </Routes>
   );
 }
